1d313fab919950a6fc3257830998dbfd2dcdaa0c
e <jannatul0040@gmail.com>
2023-10-29 23:06:25 +0600

    Display bookings and Service image in a tabular format

---
+++ src/pages/Bookings/Bookings.jsx
@@ -0,0 +1,53 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import BookingRow from "./BookingRow";
+
+
+const Bookings = () => {
+    const { user } = useContext(AuthContext);
+    const [bookings, setBookings] = useState([]);
+    const url = `http://localhost:5000/bookings?email=${user?.email}`;
+
+    useEffect(() => {
+
+    //     // axios.get(url, {withCredentials: true})
+    //     // .then(res => {
+    //     //     setBookings(res.data);
+    //     // })
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setBookings(data))
+    }, []);
+    return (
+        <div>
+            <h2>{bookings.length}</h2>
+            <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+        {
+           bookings.map(booking =><BookingRow key={booking._id} booking={booking}></BookingRow>) 
+        }
+    </tbody>
+
+    
+  </table>
+</div>
+        </div>
+    );
+};
+
+export default Bookings;
